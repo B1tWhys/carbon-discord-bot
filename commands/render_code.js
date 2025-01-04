f02513d0fedb53791d8d2688b4d19e3b64b4b797
B1tWhys <skyarnold1@me.com>
2025-01-03 17:16:02 -0700

    Setup rendering to a file

---
+++ commands/render_code.js
@@ -0,0 +1,68 @@
+// File: generate-html.mjs
+import { ExpressiveCode, ExpressiveCodeTheme } from "expressive-code";
+import { toHtml } from "expressive-code/hast";
+import nodeHtmlToImage from "node-html-to-image";
+import fs from "fs";
+import { bundledThemes, bundledThemesInfo } from "shiki/themes.mjs";
+
+const ec = new ExpressiveCode({
+  themes: [new ExpressiveCodeTheme((await bundledThemes.dracula()).default)],
+  useDarkModeMediaQuery: false,
+});
+
+// Get base styles that should be included on the page
+// (they are independent of the rendered code blocks)
+const baseStyles = await ec.getBaseStyles();
+const themeStyles = await ec.getThemeStyles();
+const jsModules = await ec.getJsModules();
+
+// Render some example code to AST
+const { renderedGroupAst, styles: blockStyles } = await ec.render({
+  code: 'console.log("Hello world!")',
+  language: "js",
+  meta: "",
+});
+
+// Convert the rendered AST to HTML
+let htmlContent = toHtml(renderedGroupAst);
+
+// Collect styles and add them before the HTML content
+const stylesToPrepend = [];
+stylesToPrepend.push(baseStyles);
+stylesToPrepend.push(themeStyles);
+stylesToPrepend.push(...blockStyles);
+
+const styleContent = `<style> ${[...stylesToPrepend].join("")} </style>`;
+const jsContent = `<script type="module"> ${[...jsModules].join("")} </script>`;
+
+const htmlDocument = `
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    ${styleContent}
+
+    ${jsContent}
+</head>
+<body>
+    ${htmlContent}
+</body>
+</html>
+`;
+// Output HTML to the console
+console.log(htmlDocument);
+
+// Run `node generate-html.mjs` to generate the HTML file
+// and open it in the browser
+// fs.writeFileSync("index.html", htmlDocument);
+
+await nodeHtmlToImage({
+  output: "test_code.png",
+  html: htmlDocument,
+});
+
+console.log("Great success!");
